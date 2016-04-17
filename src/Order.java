cdc2b0a41ebc09ff89a4ef2cb98cc3148fb0ab65
Vladimir Ulyanov <dedushka.lenin@gmail.com>
2016-04-18 07:28:23 +0800

    todos and instructions

--- src/Order.java
+++ src/Order.java
@@ -3,8 +3,21 @@
  * Заказ
  */
 public class Order {
+    /**
+     * Добавить одно блюдо в заказ.
+     *
+     * todo TESTS!
+     * todo Блюдо должно быть из меню.
+     * todo Блюдо должно быть не null.
+     *
+     * @param meal блюдо из меню
+     */
     public void addMeal(Meal meal){
         //todo
         throw new UnsupportedOperationException();
     }
+
+    //todo добавить возможность добавления нескольких порций  одного блюда, например, два чая.
+    //todo addMeal(teaMeal, 2)
+    //todo TESTS!
 }
